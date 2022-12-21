dff02aef987f9aa706ecca400f22eb419a4afdfe
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2022-12-21 22:48:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// var table = document.createElement('table');
+// for (var i = 1; i < 4; i++) {
+//     var tr = document.createElement('tr');
+
+//     var td1 = document.createElement('td');
+//     var td2 = document.createElement('td');
+
+//     var text1 = document.createTextNode('Text1');
+//     var text2 = document.createTextNode('Text2');
+
+//     td1.appendChild(text1);
+//     td2.appendChild(text2);
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+
+//     table.appendChild(tr);
+// }
+// document.body.appendChild(table);
+
+
+// let table = document.createElement("table");
+// let tr = document.createElement("tr")
+// let td = document.createElement("td")
+// td = prompt()
+// tr = prompt()
+
+// let text = document.createElement("text");
+// text = prompt;
+
+let table = document.createElement("table");
+
+td = prompt()
+tr = prompt()
+
+function creationOfTables(creation) {
+    for (let i = 1; i < creation.lenght; i++) {
+        prompt("")
+
+        // let table = document.createElement("table");
+        let tr = document.createElement("tr")
+        let td = document.createElement("td")
+        td = prompt()
+        tr = prompt()
+
+        let text = document.createElement("text");
+        text = prompt;
+        td.appendChild(text)
+        tr.appendChild(td)
+        table.appendChild(tr)
+        
+
+    }
+}
+document.body.appendChild(table)
